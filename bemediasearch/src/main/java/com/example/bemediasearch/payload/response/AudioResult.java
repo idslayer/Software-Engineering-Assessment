399c82f0e156c971f09182f559a99938a9133038
idslayer <29697148@students.lincoln.ac.uk>
2025-04-24 05:18:43 +0700

    [BE] fix JsonProperty

--- bemediasearch/src/main/java/com/example/bemediasearch/payload/response/AudioResult.java
+++ bemediasearch/src/main/java/com/example/bemediasearch/payload/response/AudioResult.java
@@ -1,19 +1,20 @@
 package com.example.bemediasearch.payload.response;
 
 import com.example.bemediasearch.dtos.AudioSet;
-import com.example.bemediasearch.dtos.AudioTag;
 import com.example.bemediasearch.dtos.Tag;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AudioResult {
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("title")
     private String title;
 
     @JsonProperty("indexed_on")
@@ -22,12 +23,16 @@ public class AudioResult {
     @JsonProperty("foreign_landing_url")
     private String foreignLandingUrl;
 
+    @JsonProperty("url")
     private String url;
+
+    @JsonProperty("creator")
     private String creator;
 
     @JsonProperty("creator_url")
     private String creatorUrl;
 
+    @JsonProperty("license")
     private String license;
 
     @JsonProperty("license_version")
@@ -36,23 +41,40 @@ public class AudioResult {
     @JsonProperty("license_url")
     private String licenseUrl;
 
+    @JsonProperty("provider")
     private String provider;
+
+    @JsonProperty("source")
     private String source;
+
+    @JsonProperty("category")
     private String category;
+
+    @JsonProperty("genres")
     private List<String> genres;
+
+    @JsonProperty("filesize")
     private Integer filesize;
+
+    @JsonProperty("filetype")
     private String filetype;
+
+    @JsonProperty("tags")
     private List<Tag> tags;
+
+    @JsonProperty("attribution")
     private String attribution;
 
     @JsonProperty("fields_matched")
     private List<String> fieldsMatched;
 
+    @JsonProperty("mature")
     private boolean mature;
 
     @JsonProperty("audio_set")
     private AudioSet audioSet;
 
+    @JsonProperty("duration")
     private Integer duration;
 
     @JsonProperty("bit_rate")
@@ -61,6 +83,7 @@ public class AudioResult {
     @JsonProperty("sample_rate")
     private Integer sampleRate;
 
+    @JsonProperty("thumbnail")
     private String thumbnail;
 
     @JsonProperty("detail_url")
@@ -69,6 +92,7 @@ public class AudioResult {
     @JsonProperty("related_url")
     private String relatedUrl;
 
+    @JsonProperty("waveform")
     private String waveform;
 
     @JsonProperty("unstable__sensitivity")
